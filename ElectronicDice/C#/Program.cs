36746a91885d41cd7a85d922e841923377b9ae82
Experiment-626 <aron.phillips@ghielectronics.com>
2019-05-10 12:15:18 -0400

    ElectronicDice: Added an extension class to allow ClearPart() to function. ClearPart() was accidentally removed in the 1.0.0 version of the BrainPad library.

--- ElectronicDice/C#/Program.cs
+++ ElectronicDice/C#/Program.cs
@@ -70,4 +70,16 @@ namespace ElectronicDice {
             }
         }
     }
+
+    public static class Ext
+    {
+        public static void ClearPart(this GHIElectronics.TinyCLR.BrainPad.Display self, int x, int y, int width, int height)
+        {
+            if (x == 0 && y == 0 && width == BrainPad.Display.Width && height == BrainPad.Display.Height)
+                self.Clear();
+            for (var lx = x; lx < width + x; lx++)
+                for (var ly = y; ly < height + y; ly++)
+                    self.ClearPoint(lx, ly);
+        }
+    }
 }
