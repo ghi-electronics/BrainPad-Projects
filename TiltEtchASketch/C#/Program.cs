b1e975cc9ed5430d334307a63788f9ee29211481
Experiment-626 <aron.phillips@ghielectronics.com>
2019-05-10 09:09:20 -0400

    TiltEtchASketch: Added an extension class to allow ClearPart() to function as the original code was created.

--- TiltEtchASketch/C#/Program.cs
+++ TiltEtchASketch/C#/Program.cs
@@ -33,4 +33,16 @@ namespace TiltEtchASketch {
             }
         }
     }
+
+    public static class Ext
+    {
+        public static void ClearPart(this GHIElectronics.TinyCLR.BrainPad.Display self, int x, int y, int width, int height)
+        {
+            if (x == 0 && y == 0 && width == BrainPad.Display.Width && height == BrainPad.Display.Height)
+                self.Clear();
+            for (var lx = x; lx < width + x; lx++)
+                for (var ly = y; ly < height + y; ly++)
+                    self.ClearPoint(lx, ly);
+        }
+    }
 }
